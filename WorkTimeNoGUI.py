0a530d44c0aa830b7e20226d718a00edd8c414c8
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-03-23 15:21:35 -0700

    Bug Fixes
    Just some in the calculator and itinerary.

--- WorkTimeNoGUI.py
+++ WorkTimeNoGUI.py
@@ -1,34 +1,94 @@
-def iteneraryMaker():
-    pass
+import sys
 
+global completeitinerary
+global completecalculator
+global Dict
+Dict = {}
 
-def actual():
-    pass
-
-def nonactual():
+def calculator():
     salary = float(input('Hello! Please enter your per hour salary here...'))
     if salary <= 0:
         print("Error! Values out of scope!")
-        main()
+        calculator()
     startTime = int(input('Enter your starting time in military time without colons!'))
+    if startTime <= 0:
+        print("Error! Values out of scope!")
+        calculator()
+    elif startTime >= 2400:
+        print("Error! Values out of scope!")
+        calculator()
     endTime = int(input('Enter your ending time in military time without colons!'))
+    if endTime <= 0:
+        print("Error! Values out of scope!")
+        calculator()
+    elif startTime >= 2400:
+        print("Error! Values out of scope!")
+        calculator()
     formula1 = endTime - startTime
     if formula1 <= 0:
         print("Error! Values out of scope!")
-        main()
+        calculator()
+    elif formula1 >= 2400:
+        print("Error! Values out of scope!")
+        calculator()
     formula2 = formula1 / 60
     formula3 = formula2 * salary
     rounder = round(formula3, 2)
     print("Your salary is about...")
     print(rounder)
     print("Dollars!")
-    iteneraryMaker()
+    main()
+
+def itinerarymaker():
+    global item, time, dayDue, moneyMade
+    firstTime = True
+    stop = int(input("Would you like to continue? 1 for yes, 2 for no."))
+    counter = 0
+    while True:
+        if stop == 1:
+            if firstTime == True:
+                counter += 1
+                item = str(input("Enter your item's name..."))
+                time = float(input("About how many hours will this take you?"))
+                dayDue = str(input("Enter the day it is due..."))
+                bonus = int(input("Is there a bonus with completion? 1 for yes, 2 for no"))
+                if bonus == 1:
+                    moneyMade = str(input("Enter how much the bonus is..."))
+                    Dict['To Do'] = {'name': item, 'how long this will take': time, 'Day it needs to be done': dayDue,
+                                     'bonus': moneyMade}
+                    itinerarymaker()
+                else:
+                    itinerarymaker()
+            else:
+                counter += 1
+                stop = int(input("Would you like to continue? 1 for yes, 2 for no."))
+                item = str(input("Enter your item's name..."))
+                time = int(input("About how many hours will this take you?"))
+                dayDue = str(input("Enter the day it is due..."))
+                bonus = int(input("Is there a bonus with completion? 1 for yes, 2 for no"))
+                if bonus == 1:
+                    moneyMade = str(input("Enter how much the bonus is..."))
+                    countered = str(counter)
+                    Dict['To Do' + countered] = {'name': item, 'how long this will take': time, 'Day it needs to be done': dayDue,
+                                     'bonus': moneyMade}
+                    itinerarymaker()
+                else:
+                    itinerarymaker()
+
+        else:
+            countered = str(counter)
+            Dict['To Do' + countered] = {'name': item, 'how long this will take': time, 'Day it needs to be done': dayDue}
+            print(Dict)
+            main()
+
 def main():
-    choice = int(input("Press 1 for actual date & time, press 2 for non-current date & time."))
+    choice = int(input("Press 1 for itinerary, press 2 for salary calculator, press 3 to exit."))
     if choice == 1:
-        actual()
+        itinerarymaker()
+    elif choice == 2:
+        calculator()
     else:
-        nonactual()
-    
+        sys.exit("Thank you!")
+
 
 main()
