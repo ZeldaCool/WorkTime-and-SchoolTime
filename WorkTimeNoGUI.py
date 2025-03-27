d97492728d80c76ad22eb3fea228b7e201beb208
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-03-27 15:52:18 -0700

    Update WorkTimeNoGUI.py

--- WorkTimeNoGUI.py
+++ WorkTimeNoGUI.py
@@ -6,35 +6,13 @@ firstuse = True
 
 def calculator():
     salary = float(input('Hello! Please enter your per hour salary here...'))
-    if salary <= 0:
-        print("Error! Values out of scope!")
-        calculator()
     startTime = int(input('Enter your starting time in military time without colons!'))
-    if startTime <= 0:
-        print("Error! Values out of scope!")
-        calculator()
-    elif startTime >= 2400:
-        print("Error! Values out of scope!")
-        calculator()
     endTime = int(input('Enter your ending time in military time without colons!'))
-    if endTime <= 0:
-        print("Error! Values out of scope!")
-        calculator()
-    elif startTime >= 2400:
-        print("Error! Values out of scope!")
-        calculator()
     formula1 = endTime - startTime
-    if formula1 <= 0:
-        print("Error! Values out of scope!")
-        calculator()
-    elif formula1 >= 2400:
-        print("Error! Values out of scope!")
-        calculator()
-    formula2 = formula1 / 60
+    formula2 = formula1/100
     formula3 = formula2 * salary
-    rounder = round(formula3, 2)
     print("Your salary is about...")
-    print(rounder)
+    print(formula3)
     print("Dollars!")
     main()
 
@@ -72,5 +50,5 @@ def main():
         sys.exit("Thank you!")
     elif choice == 3:
         itinerary(it)
-        
+
 main()
